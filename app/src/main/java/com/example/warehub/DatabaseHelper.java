20a8a226b7f61f8de2a80dfe50403e732d54c7b4
dharmesh-kashyap <dharmeshkashyap46@gmail.com>
2024-10-24 20:11:27 +0530

    Stage II

--- app/src/main/java/com/example/warehub/DatabaseHelper.java
+++ app/src/main/java/com/example/warehub/DatabaseHelper.java
@@ -256,18 +256,19 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     // Method to get products with quantity less than 3
+    // Method to get products with quantity less than 3
     public ArrayList<Product> getLowQuantityProducts() {
         ArrayList<Product> lowQuantityProducts = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_NAME + " WHERE " + COL_4 + " < 3", null);
+        Cursor cursor = db.rawQuery("SELECT * FROM " + TABLE_PRODUCTS + " WHERE " + COLUMN_QUANTITY + " < 3", null);
 
         if (cursor.moveToFirst()) {
             do {
-                int id = cursor.getInt(cursor.getColumnIndexOrThrow(COL_1));
-                String name = cursor.getString(cursor.getColumnIndexOrThrow(COL_2));
-                String code = cursor.getString(cursor.getColumnIndexOrThrow(COL_3));
-                int quantity = cursor.getInt(cursor.getColumnIndexOrThrow(COL_4));
-                double price = cursor.getDouble(cursor.getColumnIndexOrThrow(COL_5));
+                int id = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_PRODUCT_ID));
+                String name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_PRODUCT_NAME));
+                String code = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_PRODUCT_CODE));
+                int quantity = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_QUANTITY));
+                double price = cursor.getDouble(cursor.getColumnIndexOrThrow(COLUMN_PRICE));
 
                 Product product = new Product(id, name, code, quantity, price);
                 lowQuantityProducts.add(product);
@@ -276,6 +277,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         cursor.close();
         return lowQuantityProducts;
     }
+
 
 }
 
